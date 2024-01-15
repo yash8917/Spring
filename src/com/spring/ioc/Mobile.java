50da9240abc6e4328bae661fad700ff3e3b6b5fb
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2024-01-15 10:31:50 +0530

    Final Update of IOC Container

--- src/com/spring/ioc/Mobile.java
+++ src/com/spring/ioc/Mobile.java
@@ -1,4 +1,6 @@
 package com.spring.ioc;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Mobile {
     public static void main(String[] args) {
@@ -10,7 +12,14 @@ public class Mobile {
 //        vodaphone.calling();
 //        vodaphone.data();
 
-        Sim sim= new Airtel();
+//        Sim sim= new Airtel();
+//        sim.calling();
+//        sim.data();
+
+//        using IOC Container
+        ApplicationContext applicationContext =new ClassPathXmlApplicationContext("beans.xml");
+        System.out.println("Config loaded");
+        Sim sim = applicationContext.getBean("Sim" , Sim.class);
         sim.calling();
         sim.data();
     }
